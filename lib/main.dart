5f268be76536757565fa943c6278c663e687e119
wildanarkan <cillmystic@gmail.com>
2024-01-14 01:00:59 +0700

    Pengenalan Layouting

--- lib/main.dart
+++ lib/main.dart
@@ -10,22 +10,132 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 240, 184, 132),
-          title: Text("Wchicken"),
-        ),
-        body: 
-        Center(
-          child: Container(
-            width: 100,
-            height: 100,
-            color: Colors.amber,
-            child: Center(
-              child: Image.asset("assets/image/wchicken.png"),
-              )
-            ),
-        ),
-      ),
+          appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 240, 184, 132),
+            title: Text("Wchicken"),
+          ),
+          body: GridView(
+            padding: EdgeInsets.all(10),
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10),
+            children: [
+              Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(
+                    child: Image.asset("assets/image/wchicken.png"),
+                  )
+                ),
+              Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(
+                    child: Image.asset("assets/image/wchicken.png"),
+                  )
+                ),
+              Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(
+                    child: Image.asset("assets/image/wchicken.png"),
+                  )
+                ),
+              Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(
+                    child: Image.asset("assets/image/wchicken.png"),
+                  )
+                ),
+              Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(
+                    child: Image.asset("assets/image/wchicken.png"),
+                  )
+                ),
+              Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(
+                    child: Image.asset("assets/image/wchicken.png"),
+                  )
+                ),
+              Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(
+                    child: Image.asset("assets/image/wchicken.png"),
+                  )
+                ),
+              Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(
+                    child: Image.asset("assets/image/wchicken.png"),
+                  )
+                ),
+              Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(
+                    child: Image.asset("assets/image/wchicken.png"),
+                  )
+                ),
+              Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(
+                    child: Image.asset("assets/image/wchicken.png"),
+                  )
+                ),
+              Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(
+                    child: Image.asset("assets/image/wchicken.png"),
+                  )
+                ),
+              Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(
+                    child: Image.asset("assets/image/wchicken.png"),
+                  )
+                ),
+              Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(
+                    child: Image.asset("assets/image/wchicken.png"),
+                  )
+                ),
+              Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.amber,
+                  child: Center(
+                    child: Image.asset("assets/image/wchicken.png"),
+                  )
+                ),
+            ],
+          )),
     );
   }
 }
