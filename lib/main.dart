962bc3aab13ad7776b4969f8f34e9e0d6a18b81a
wildanarkan <cillmystic@gmail.com>
2024-01-14 13:38:33 +0700

    Pengenalan Builder

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // const MyApp({super.key});
-
-List<Map<String, dynamic>> data = List.generate(
-  10,
-  (index) => {
-    "text" : "Cabang ke - ${index+1}",
-    "image" : "assets/image/wchicken.png",
-    "color" : Color.fromARGB(
-      255,
-      200 + Random().nextInt(256),
-      200 + Random().nextInt(256),
-      200 + Random().nextInt(256),)
-  } );
+  const  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -30,21 +18,27 @@ List<Map<String, dynamic>> data = List.generate(
             backgroundColor: Color.fromARGB(255, 240, 184, 132),
             title: Text("Wchicken"),
           ),
-          body: GridView(
+          body: GridView.builder(
             padding: EdgeInsets.all(10),
-            gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10),
-            children:
-            data.map((e) =>
+            itemCount: 20,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10
+              ),
+            itemBuilder: (context, index) => 
             ImageBox(
-              cabang: e["text"],
-              image: e["image"],
-              background: e["color"]
-              )).toList(),
-          )),
+              cabang: "Cabang Ke - ${index+1}",
+              image: "assets/image/wchicken.png",
+              background: Color.fromARGB(
+                255,
+                100+Random().nextInt(256),
+                100+Random().nextInt(256),
+                100+Random().nextInt(256)
+                )
+              )
+            ,)
+      )
     );
   }
 }
