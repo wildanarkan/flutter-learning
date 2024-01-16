3297704d190740317d8dacc9f39650a5e1eade8c
wildanarkan <cillmystic@gmail.com>
2024-01-16 21:20:08 +0700

    Latihan Widget 1

---
+++ lib/widgets/latihan/latihan1.dart
@@ -0,0 +1,60 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:latihan_flutter/widgets/image_box.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color.fromARGB(255, 240, 184, 132),
+            title: Text("Wchicken"),
+          ),
+          body: ListView.builder(
+            padding: EdgeInsets.all(20),
+            itemCount: 50,
+            itemBuilder: (context, index) {
+            if(index % 2 == 0){
+              return Padding(
+                padding:  EdgeInsets.only(bottom: 25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 150,
+                      color: Colors.blue
+                      ),
+                      SizedBox(height: 10),
+                      Text("Hello ${index+1}",
+                      style: TextStyle(fontSize: 25) ,)
+                  ],
+                ),);
+            }else{
+               return Padding(
+                padding:  EdgeInsets.only(bottom: 25),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 150,
+                      color: Colors.amber
+                      ),
+                      SizedBox(height: 10),
+                      Text("Hello ${index+1}",
+                      style: TextStyle(fontSize: 25) ,)
+                  ],
+                ),);
+            }
+          },)
+      )
+    );
+  }
+}
+
