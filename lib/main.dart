ed410626fe802dd809c29605ede941293ea3cada
wildanarkan <cillmystic@gmail.com>
2024-01-17 20:39:06 +0700

    Memahami Stateless & Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,6 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:latihan_flutter/widgets/image_box.dart';
-import 'package:latihan_flutter/widgets/latihan/latihan.dart';
-import 'package:latihan_flutter/widgets/latihan/latihan1.dart';
-import './widgets/latihan/latihan2.dart';
-import './widgets/latihan/latihan3.dart';
-import './widgets/latihan/latihan4.dart';
+import './widgets/latihan/stateful.dart';
 
-void main() {
-  runApp(MyApp());
+void main(){
+  runApp(AppStateful());
 }
-
-class MyApp extends StatelessWidget {
-  const  MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: latihan4()
-    );
-  }
-}
-
